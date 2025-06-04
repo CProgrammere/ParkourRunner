ffe33909e4551ed3675f3224fddef228bc26647c
johanberg165 <johanberg165@gmail.com>
2025-06-04 11:23:42 +0200

    Created MoveAction

--- Assets/Scripts/PlayerActionHandler.cs
+++ Assets/Scripts/PlayerActionHandler.cs
@@ -2,7 +2,26 @@ using UnityEngine;
 
 public class PlayerActionHandler : ActionHandler
 {
+    [field: SerializeField] public PlayerControls PlayerControls { get; private set; }
+    [field: SerializeField] public Animator Animator { get; private set; }
+
     public PlayerAction playerAction;
+
+    private void Awake()
+    {
+        SwitchAction(new PlayerMoveAction(this));
+        PlayerControls = new PlayerControls();
+    }
+
+    private void OnEnalbe()
+    {
+        PlayerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        PlayerControls.Disable();
+    }
 
     public void SwitchAction(PlayerAction playerAction)
     {
--- Assets/Scripts/PlayerMoveAction.cs
+++ Assets/Scripts/PlayerMoveAction.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class PlayerMoveAction : PlayerAction
 {
+    private Vector2 moveDirection;
+    private int moveHash = Animator.StringToHash("MoveBelndTree");
+
     public override void Enter()
     {
-
+        PlayerActionHandler.Animator.CrossFadeInFixedTime(moveHash, 0.1f);
     }
 
     public override void Tick(float deltaTime)
     {
-
+        moveDirection = PlayerActionHandler.PlayerControls.Player.Walk.ReadValue<Vector2>();
+        moveDirection.Normalize();
+        PlayerActionHandler.Animator.SetFloat("X", moveDirection.x);
+        PlayerActionHandler.Animator.SetFloat("Y", moveDirection.y);
     }
 
     public override void Exit()
